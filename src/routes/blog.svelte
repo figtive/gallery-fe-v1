a48699e944954d9ae0f45f2a3df0e3f2acbd63d6
Danny August Ramaputra <daystram@gmail.com>
2021-11-02 21:01:51 +1000

    chore: stubbed pages

---
+++ src/routes/blog.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import type { Blog } from '$lib/dtos';
+
+	let blogs: Blog[] = [];
+</script>
+
+<main>
+	<div class="container">
+		<div class="head">
+			<h1 class="page-title">Blog</h1>
+		</div>
+		<div class="body">
+			<div class="list">
+				{#each blogs as blog (blog.id)}{/each}
+			</div>
+		</div>
+	</div>
+	<div class="glow-1" />
+</main>
+
+<style lang="css">
+	.list {
+		display: flex;
+		flex-wrap: wrap;
+		margin: 0 -2rem;
+	}
+
+	.list > :global(*) {
+		margin: 2rem;
+	}
+
+	.glow-1 {
+		position: fixed;
+		z-index: -1;
+		width: 80vh;
+		height: 80vh;
+		bottom: 0;
+		right: 0;
+		border-radius: 100%;
+		background: #f9eab3;
+		filter: blur(20vh);
+		transform: translate(50%, 50%);
+	}
+</style>
---
+++ src/routes/leaderboard.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<main>
+	<div class="container">
+		<div class="head">
+			<h1 class="page-title">Leaderboard</h1>
+		</div>
+		<div class="body">
+			<div class="list" />
+		</div>
+	</div>
+</main>
+
+<style lang="css">
+	.list {
+		display: flex;
+		flex-wrap: wrap;
+		margin: 0 -2rem;
+	}
+
+	.list > :global(*) {
+		margin: 2rem;
+	}
+</style>
