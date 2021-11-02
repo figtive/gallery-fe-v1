e148064f4b90012279df3b289db742a26428f180
Danny August Ramaputra <daystram@gmail.com>
2021-11-02 22:22:17 +1000

    feat: completed project item

--- src/lib/components/ProjectItem.svelte
+++ src/lib/components/ProjectItem.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import { ProjectFieldTypeLabel } from '$lib/constant';
+
 	import type { Project } from '$lib/dtos';
+	import Tag from './Tag.svelte';
 
 	export let project: Project;
 	export let onClick: () => void = () => {};
@@ -17,6 +20,14 @@
 	<div class="content">
 		<p class="name">{project.name}</p>
 		<p class="team">{project.team}</p>
+		<div class="tags">
+			<Tag color="secondary">{new Date(project.createdAt).getFullYear()}</Tag>
+			<Tag color={project.active ? 'success' : 'disabled'}>
+				{project.active ? 'Active' : 'Archived'}
+			</Tag>
+			<Tag color="info">{ProjectFieldTypeLabel[project.field]}</Tag>
+		</div>
+		<p class="description">{project.description}</p>
 	</div>
 </div>
 
@@ -24,10 +35,9 @@
 	.card {
 		display: flex;
 		flex-direction: column;
-		width: 256px;
-		height: 256px;
+		width: 360px;
 		padding: 0;
-		border-radius: 0.5rem;
+		border-radius: 1.5rem;
 		overflow: hidden;
 		cursor: pointer;
 		box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.25);
@@ -41,7 +51,7 @@
 
 	.card .image {
 		background-color: #eee;
-		height: 60%;
+		height: 220px;
 		overflow: hidden;
 	}
 
@@ -56,11 +66,27 @@
 	.card .content {
 		background-color: #fff;
 		flex-grow: 1;
-		padding: 0.5rem 0.75rem;
+		padding: 1rem 1.2rem 1.2rem 1.2rem;
 	}
 
 	.card .content .name {
 		font-size: 1.5rem;
 		font-weight: 600;
+		margin-bottom: 4px;
+	}
+
+	.card .content .team {
+		margin-bottom: 12px;
+	}
+
+	.card .content .tags {
+		display: flex;
+		flex-wrap: wrap;
+		margin: -4px -4px;
+		margin-bottom: 8px;
+	}
+
+	.card .content .tags > :global(*) {
+		margin: 4px;
 	}
 </style>
