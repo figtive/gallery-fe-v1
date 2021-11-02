7be473d65e92d3ee2de03dd8638c1b57d044bf20
Danny August Ramaputra <daystram@gmail.com>
2021-11-03 03:48:09 +1000

    feat: added button icons

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -5,10 +5,20 @@
 	export let onClick: () => void = () => {};
 	export let className: string = '';
 	export let color: Color = 'primary';
+	export let beforeIcon: string = '';
+	export let afterIcon: string = '';
 </script>
 
 <button {type} on:click={onClick} class={className} style="--color: {colorSet[color]}">
-	<slot />
+	{#if beforeIcon}
+		<span class="material-icons before-icon">{beforeIcon}</span>
+	{/if}
+	<span class="content">
+		<slot />
+	</span>
+	{#if afterIcon}
+		<span class="material-icons after-icon">{afterIcon}</span>
+	{/if}
 </button>
 
 <style lang="css">
@@ -18,12 +28,16 @@
 		border: 0px solid transparent;
 		border-radius: 120px;
 		cursor: pointer;
-		padding: 0 1.5rem;
+		padding: 0;
 		height: 40px;
 		font-family: 'Open Sans', sans-serif;
 		font-weight: 600;
 		background: var(--color);
 		transition: 100ms;
+		display: flex;
+		flex-direction: row;
+		flex-shrink: 0;
+		align-items: center;
 	}
 
 	button:hover,
@@ -33,5 +47,22 @@
 
 	button:active {
 		filter: brightness(0.8);
+	}
+
+	.before-icon {
+		padding-left: 1rem;
+		margin-right: -0.75rem;
+		font-size: 20px;
+	}
+
+	.content {
+		padding: 0 1.5rem;
+		font-size: 1rem;
+	}
+
+	.after-icon {
+		padding-right: 1rem;
+		margin-left: -0.75rem;
+		font-size: 20px;
 	}
 </style>
