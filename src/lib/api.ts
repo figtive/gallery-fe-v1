b6533c632792f9be35787b1d79f151496d43f4d9
Danny August Ramaputra <daystram@gmail.com>
2021-11-03 19:53:52 +1000

    refactor: updated api handling

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import { get } from 'svelte/store';
 import { auth } from './auth';
-import type { APIResponse } from './dtos';
+import type { APIResponse, Project } from './dtos';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL || '';
 
@@ -8,33 +8,44 @@ const withAuth = (token?: string): HeadersInit => ({
 	Authorization: `Bearer ${token || get(auth.getToken())}`
 });
 
-const handleResponse = (response: Response) => {
+const handleResponse = async <T>(response: Response) => {
+	const jsonResponse: APIResponse<T> = await response.json();
 	if (!response.ok) {
 		if (response.status === 401) {
-			new auth.reauthenticate();
+			auth.deauthenticate();
+			throw new Error('Please sign in again');
 		}
-		throw new Error(response.statusText);
+		throw new Error(jsonResponse.error);
 	}
-	return response.json();
+	return jsonResponse.data;
 };
 
 const api = {
 	auth: {
-		login(token: string): Promise<APIResponse<undefined>> {
+		login(token: string): Promise<void> {
 			return fetch(`${BASE_URL}/api/v1/auth/login`, {
 				method: 'POST',
 				headers: {
 					...withAuth(token)
 				}
-			}).then(handleResponse);
-		},
-		logout(): Promise<APIResponse<undefined>> {
-			return fetch(`${BASE_URL}/api/v1/auth/logout`, {
-				method: 'POST',
-				headers: {
-					...withAuth()
-				}
-			}).then(handleResponse);
+			}).then((resp) => handleResponse<void>(resp));
+		}
+	},
+	coursework: {
+		project: {
+			getOne(id: string): Promise<Project> {
+				return fetch(`${BASE_URL}/api/v1/coursework/project/${id}`, {
+					method: 'GET'
+				}).then((resp) => handleResponse<Project>(resp));
+			},
+			getAll(query: string, course: string, field: string): Promise<Project[]> {
+				return fetch(
+					`${BASE_URL}/api/v1/coursework/project?${new URLSearchParams({ query, course, field })}`,
+					{
+						method: 'GET'
+					}
+				).then((resp) => handleResponse<Project[]>(resp));
+			}
 		}
 	}
 };
