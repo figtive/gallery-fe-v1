1e8cf1fe045d2cc65fe1e0ab5fc7ce5668f24051
Danny August Ramaputra <daystram@gmail.com>
2021-11-03 21:13:54 +1000

    chore: fixed lint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	/* eslint-disable @typescript-eslint/ban-ts-comment */
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	import { AuthManager, Response } from '$lib/auth';
